9b90953b272ecc99f0f61374259b221f1ffbd8e4
Hubnotch <love2ekene@gmail.com>
2021-05-16 13:51:54 +0100

    Created setup files for the project

---
+++ app.js
@@ -0,0 +1,8 @@
+/* 
+These are two algorithm challenges from zuri frontend
+development program using javascript stack. 
+-- The first is the Temperature conversion algorithm
+-- The Fizzbuzz like challenge
+*/
+
+/* Temperature conversion algorithm */
