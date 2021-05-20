987efcb0a0da9c10a1cccf2acaa12e0c94cdbc99
Hubnotch <love2ekene@gmail.com>
2021-05-20 17:04:43 +0100

    added additional logic to the algorithm

--- app.js
+++ app.js
@@ -43,3 +43,40 @@ Use the following cases as guides:
 - checkYuGiOh("5") should return [1, "yu", "gi", "yu", "oh"]
 - checkYuGiOh("fizzbuzz is meh") should return `invalid parameter: "fizzbuzz is meh"`
 */
+
+let checkYuGiOh = (n) => {
+  let numIndex = Number(n);
+  if (numIndex) {
+    let arr = [];
+    for (let i = 1; i <= numIndex; i++) {
+      
+      if (i % 2 === 0) {
+        arr.push("yu");
+      }
+      else if (i % 3 === 0) {
+        arr.push("gi");
+      }
+      else if (i % 5 === 0) {
+        arr.push("oh");
+      }
+      else if (i % 2 === 0 && i % 5 === 0) {
+        arr.push("yu-oh");
+      }
+      else if (i % 3 === 0 && i % 5 === 0) {
+        arr.push("yu-gi");
+      }
+      else if (i % 2 === 0 && i % 3 === 0 && i % 5 === 0) {
+        arr.push("yu-gi-oh");
+      } else {
+        arr.push(i);
+      }
+     
+    }
+    return arr;
+    
+  } else {
+    return `invalid parameter : ${n}`;
+  }
+};
+
+console.log(checkYuGiOh('5'));
